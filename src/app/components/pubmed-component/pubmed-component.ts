e243c756a608cca8c8ae14a21995811bbe347a89
Baltazar <baltazarottonello@gmail.com>
2025-09-21 18:06:44 -0300

    this commit is incomplete, pushing only because i need to switch to other architecture

--- src/app/components/pubmed-component/pubmed-component.ts
+++ src/app/components/pubmed-component/pubmed-component.ts
@@ -12,7 +12,13 @@ import {
   ViewChild,
   viewChild,
 } from "@angular/core";
-import { PubMedArticle } from "../../types/pubmedArticle";
+import {
+  PubMedArticle,
+  PubmedAuthor,
+  PubmedLang,
+  PubmedMesh,
+  PubmedPubType,
+} from "../../types/pubmedArticle";
 import { PubMedService } from "../../services/pubMedService.service";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
@@ -23,6 +29,7 @@ import {
   Validators,
 } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { concatMap, delay, from, map, of, switchMap } from "rxjs";
 
 @Component({
   selector: "app-pubmed-component",
@@ -44,8 +51,10 @@ export class PubmedComponent {
   renderer = inject(Renderer2);
   ids = signal("");
   articlesSet = signal<Document | null>(null);
+  formattedArticles = signal<PubMedArticle[]>([]);
   csvUploaded = signal(false);
   csvConverted = signal(false);
+  loading = signal(false);
 
   pmidFormControl = new FormControl("", [Validators.required]);
   toCsvFormControl = new FormControl("", [Validators.required]);
@@ -93,22 +102,79 @@ export class PubmedComponent {
 
   //TODO: if more than about 200 UIDs are to be provided, the request should be made using the HTTP POST method.
   searchPmid(pmid: string) {
-    if (this.pmidFormControl.invalid) {
+    if (this.pmidFormControl.invalid && !this.csvUploaded()) {
       this.pmidFormControl.markAsTouched();
       return;
     }
-    this.pubmedService.getArticles(pmid).subscribe({
-      next: (res: any) => {
-        const domParser = new DOMParser();
-        const xml = domParser.parseFromString(res, "text/xml");
-        this.articlesSet.set(xml);
-        this.formatArticleSet();
-      },
-      error: (e) => console.error(e),
-    });
+
+    if (this.ids().length >= 200) {
+      const idsArray = this.ids().split(",");
+      const chunkSize = idsArray.length / 100 > 1 ? 100 : idsArray.length;
+      const chunksQty = Math.ceil(idsArray.length / chunkSize);
+
+      const chunks = [];
+
+      let counter = 0;
+      for (let i = 0; i < chunksQty; i++) {
+        const chunk = [];
+        for (let j = 0; j < chunkSize; j++) {
+          if (counter >= idsArray.length) break;
+          chunk.push(idsArray[counter]);
+          counter++;
+        }
+        chunks.push(chunk);
+      }
+      this.loading.set(true);
+      from(chunks)
+        .pipe(
+          concatMap((chunk) =>
+            this.pubmedService.getArticles(chunk.join(","), chunk.length).pipe(
+              map((res: any) => {
+                const domParser = new DOMParser();
+                const xml = domParser.parseFromString(res, "text/xml");
+                this.articlesSet.set(xml);
+                this.formatArticleSet();
+                return this.formattedArticles();
+              }),
+              concatMap((articles) =>
+                this.pubmedService.createArticle(articles)
+              ),
+              delay(500)
+            )
+          )
+        )
+        .subscribe({
+          next: (res: any) => {
+            console.log("insertado");
+          },
+          error: (e) => {
+            console.error(e);
+            this.loading.set(false);
+          },
+          complete: () => this.loading.set(false),
+        });
+    } else {
+      this.pubmedService
+        .getArticles(pmid)
+        .pipe(
+          map((res: any) => {
+            const domParser = new DOMParser();
+            const xml = domParser.parseFromString(res, "text/xml");
+            this.articlesSet.set(xml);
+            this.formatArticleSet();
+            return this.formattedArticles();
+          }),
+          switchMap((articles) => this.pubmedService.createArticle(articles))
+        )
+        .subscribe({
+          next: (res: any) => {},
+          error: (e) => console.error(e),
+        });
+    }
   }
 
   private formatArticleSet() {
+    //TODO: handle PubmedBookArticle as well
     const articleSet = this.articlesSet()?.querySelectorAll("PubmedArticle")!;
     const articlesXml = [];
     const articles: PubMedArticle[] = [];
@@ -117,26 +183,197 @@ export class PubmedComponent {
     }
 
     for (let [_, value] of articlesXml.entries()) {
-      const pmid = parseInt(value.querySelector("PMID")?.innerHTML!);
-      const title = value.querySelector("ArticleTitle")?.innerHTML!;
+      const pmid = parseInt(value.querySelector("PMID")?.textContent?.trim()!);
+      const title = value.querySelector("ArticleTitle")?.textContent?.trim()!;
       const year = parseInt(
-        value.querySelector("DateRevised")!.querySelector("Year")?.innerHTML!
+        value.querySelector("DateRevised > Year")?.textContent?.trim()!
       );
       const status = value
         .querySelector("MedlineCitation")
-        ?.attributes.getNamedItem("Status")?.value!;
-      const article = {
+        ?.attributes.getNamedItem("Status")
+        ?.value?.trim()!;
+      const doi = this.extractDoi(value);
+      const month = this.extractMonth(value);
+      const types = this.extractTypes(value);
+      const volume = this.extractVolume(value);
+      const issue = this.extractIssue(value);
+      const start_page = this.extractStartPage(value);
+      const end_page = this.extractendPage(value);
+      const abstractXmlString = this.extractAbstractXmlString(value);
+      const authors = this.extractAuthors(value);
+      const languages = this.extractLanguages(value);
+      const mesh = this.extractMesh(value);
+      const journal = this.extractJournalData(value);
+
+      const article: PubMedArticle = {
         pmid,
         title,
         year,
         status,
+        doi,
+        month,
+        types,
+        volume,
+        issue,
+        start_page,
+        end_page,
+        abstract: abstractXmlString,
+        authors,
+        langs: languages,
+        mesh,
+        journal,
       };
       articles.push(article);
+      this.formattedArticles.set(articles);
+    }
+  }
+
+  private extractDoi(xml: Element) {
+    let doi = null;
+    const doiOption1 = xml.querySelector('ELocationID[EIdType="doi"]');
+    const doiOption2 = xml.querySelector('ArticleId[IdType="doi"]');
+
+    if (doiOption1) {
+      doi = doiOption1.textContent?.trim() ?? null;
     }
 
-    this.pubmedService.createArticle(articles).subscribe({
-      next: (res: any) => console.log(res),
-      error: (e) => console.error(e),
-    });
+    if (doiOption2) {
+      doi = doiOption2.textContent?.trim() ?? null;
+    }
+
+    return doi;
+  }
+
+  private extractMonth(xml: Element) {
+    const raw = xml.querySelector("DateRevised > Month")?.textContent?.trim();
+    return raw ? parseInt(raw) : null;
+  }
+
+  private extractTypes(xml: Element) {
+    const types = Array.from(
+      xml.querySelectorAll("PublicationTypeList > PublicationType")
+    )
+      .map((type) => {
+        const name = type.textContent?.trim() ?? null;
+        return { name };
+      })
+      .filter((t) => t.name);
+
+    return types.length > 0 ? types : null;
+  }
+
+  private extractVolume(xml: Element) {
+    const volume = xml.querySelector("Volume")?.textContent?.trim();
+    return volume ? parseInt(volume) : null;
+  }
+
+  private extractIssue(xml: Element) {
+    const issue = xml.querySelector("Issue")?.textContent?.trim();
+    return issue ? parseInt(issue) : null;
+  }
+
+  private extractStartPage(xml: Element) {
+    const startPage = xml.querySelector("StartPage")?.textContent?.trim();
+    return startPage ? parseInt(startPage) : null;
+  }
+
+  private extractendPage(xml: Element) {
+    const endPage = xml.querySelector("EndPage")?.textContent?.trim();
+    return endPage ? parseInt(endPage) : null;
+  }
+
+  private extractAbstractXmlString(xml: Element) {
+    const abstract = xml.querySelector("Abstract")?.outerHTML!;
+    return abstract ?? null;
+  }
+
+  private extractAuthors(xml: Element) {
+    const authors: PubmedAuthor[] | null = [];
+    const authorsEl = xml.querySelectorAll("Author");
+
+    if (authorsEl) {
+      for (let [_, value] of authorsEl.entries()) {
+        const name = value.querySelector("ForeName")?.textContent;
+        const lastname = value.querySelector("LastName")?.textContent;
+        const orcid =
+          value.querySelector('Identifier[Source="ORCID"]')?.textContent ??
+          null;
+        const affiliations = this.extractAffiliations(value);
+        authors.push({ name, lastname, orcid, affiliations });
+      }
+      return authors;
+    } else return null;
+  }
+
+  private extractAffiliations(xml: Element) {
+    const affiliationsNodeList = xml.querySelectorAll(
+      "AffiliationInfo > Affiliation"
+    );
+
+    const affiliations = [];
+    console.log();
+    if (affiliationsNodeList.length > 0) {
+      for (let [_, aff] of affiliationsNodeList.entries()) {
+        const affiliation = aff.textContent?.trim();
+        affiliations.push({ name: affiliation });
+      }
+      return affiliations;
+    } else return null;
+  }
+
+  private extractLanguages(xml: Element) {
+    const languageEl = xml.querySelectorAll("Article > Language");
+
+    const languages: PubmedLang[] = [];
+
+    if (languageEl.length > 0) {
+      for (let [_, lang] of languageEl.entries()) {
+        const language = lang.textContent?.trim();
+        languages.push({ name: language });
+      }
+      return languages;
+    } else return null;
+  }
+
+  private extractMesh(xml: Element) {
+    const descriptorsList = xml.querySelectorAll(
+      "MeshHeadingList > MeshHeading > DescriptorName[UI]"
+    );
+
+    const meshHeadings: PubmedMesh[] = [];
+
+    if (descriptorsList.length > 0) {
+      for (let [_, descriptor] of descriptorsList.entries()) {
+        const descriptorName = descriptor.textContent?.trim();
+        const descriptorUi = descriptor.attributes
+          .getNamedItem("UI")
+          ?.value?.trim();
+        meshHeadings.push({ name: descriptorName, ui: descriptorUi });
+      }
+      return meshHeadings;
+    } else return null;
+  }
+
+  private extractJournalData(xml: Element) {
+    const journalEl = xml.querySelector("Journal");
+    const title = journalEl!.querySelector("Title")!.textContent!.trim()!;
+    const country = xml
+      .querySelector("MedlineJournalInfo > Country")
+      ?.textContent?.trim();
+    const abbr = journalEl
+      ?.querySelector("ISOAbbreviation")
+      ?.textContent?.trim();
+    const issn = journalEl?.querySelector("ISSN")?.textContent?.trim();
+    const issn_type = journalEl
+      ?.querySelector("ISSN")
+      ?.attributes.getNamedItem("IssnType")
+      ?.value.trim();
+    return {
+      title,
+      country,
+      abbr,
+      issn,
+      issn_type,
+    };
   }
 }
